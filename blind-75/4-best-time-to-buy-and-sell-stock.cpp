b6d2079b3a81d55e7dcdcd26fd4bf00551fea3e9
Shubham Shekhar Jha <jhashubhamshekhar@gmail.com>
2022-09-03 03:48:52 -0500

    easy soln to easy problem

--- blind-75/4-best-time-to-buy-and-sell-stock.cpp
+++ blind-75/4-best-time-to-buy-and-sell-stock.cpp
@@ -32,94 +32,44 @@ Constraints:
 */
 
 #include <bits/stdc++.h>
+#include <climits>
 #include <iostream>
-#include <queue>
-#include <unordered_map>
-#include <vector>
 using namespace std;
 
 class Solution {
   public:
-	int maxProfit(vector<int>& prices) {
-		int max_profit = 0;
-		// bool non_increasing_prices = check_non_increasing_prices(prices);
-		// if (non_increasing_prices) {
-		//     return max_profit;
-		// }
+    int maxProfit(vector<int>& prices) {
+        int max_profit = 0;
+        int least = INT_MAX;
+        for (int i = 0; i < prices.size(); i++) {
+            if (prices[i] < least) {
+                least = prices[i];
+            }
 
-		priority_queue<int> max_heap;
-		priority_queue<int, vector<int>, greater<int>> min_heap;
-		unordered_map<int, int> element_index_map;
-		populate_heaps_maps(prices, max_heap, min_heap, element_index_map);
-
-		while (!max_heap.empty() && !min_heap.empty()) {
-			int buy = min_heap.top();
-			int sell = max_heap.top();
-			int profit = sell - buy;
-
-			if (profit < 0) {
-				break;
-			}
-
-			if (element_index_map[sell] > element_index_map[buy]) {
-				max_profit = profit;
-				break;
-			}
-
-			if() {
-				max_heap.pop();
-			} else {
-				min_heap.pop();
-			}
-		}
-
-		return max_profit;
-	}
-
-  private:
-	bool check_non_increasing_prices(vector<int>& prices) {
-		int count_increase = 0;
-		for (int i = 0; i < prices.size() - 1; i++) {
-			if (prices[i + 1] > prices[i]) {
-				count_increase++;
-				break;
-			}
-		}
-
-		if (count_increase == 0) {
-			return true;
-		} else {
-			return false;
-		}
-	}
-
-	void populate_heaps_maps(vector<int>& prices, priority_queue<int>& max_heap,
-							 priority_queue<int, vector<int>, greater<int>>& min_heap,
-							 unordered_map<int, int>& element_index_map) {
-
-		for (int i = 0; i < prices.size(); i++) {
-			max_heap.push(prices[i]);
-			min_heap.push(prices[i]);
-			element_index_map[prices[i]] = i;
-		}
-	}
+            int profit = prices[i] - least;
+            if (profit > max_profit) {
+                max_profit = profit;
+            }
+        }
+        return max_profit;
+    }
 };
 
 int main() {
-	Solution sol;
-	int n;
-	vector<int> stock_prices;
+    Solution sol;
+    int n;
+    vector<int> stock_prices;
 
-	cout << "Enter n" << endl;
-	cin >> n;
-	for (int i = 0; i < n; i++) {
-		int temp;
-		cin >> temp;
-		stock_prices.push_back(temp);
-	}
+    cout << "Enter n" << endl;
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        int temp;
+        cin >> temp;
+        stock_prices.push_back(temp);
+    }
 
-	int res = sol.maxProfit(stock_prices);
-	cout << res << endl;
+    int res = sol.maxProfit(stock_prices);
+    cout << res << endl;
 
-	return 0;
+    return 0;
 }

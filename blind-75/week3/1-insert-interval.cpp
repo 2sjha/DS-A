9b5d3715d3b206558b47276bd7222c1a447bc99c
Shubham Shekhar Jha <jhashubhamshekhar@gmail.com>
2022-11-26 17:07:04 -0600

    week3/1 new simpler logic but too many cases to handle

--- blind-75/week3/1-insert-interval.cpp
+++ blind-75/week3/1-insert-interval.cpp
@@ -42,135 +42,84 @@ Constraints:
 using namespace std;
 
 class Solution {
-private:
-  void print_line(vector<int> &line) {
-    for (auto l : line) {
-      cout << l << " ";
-    }
-    cout << endl;
-  }
-
-  void print_map(unordered_map<string, int> &line) {
-    for (auto l : line) {
-      cout << l.first << " " << l.second << endl;
-    }
-    cout << endl;
-  }
-
-  vector<int> getInterval(string tmp_ins) {
-    int dashIdx = tmp_ins.find('-');
-    int start = stoi(tmp_ins.substr(0, dashIdx));
-    int end = stoi(tmp_ins.substr(dashIdx + 1, tmp_ins.length()));
-    vector<int> interval = {start, end};
-    return interval;
-  }
-
-public:
-  vector<vector<int>> insert(vector<vector<int>> &intervals,
-                             vector<int> &newInterval) {
-    int newInterval_start = newInterval[0];
-    int newInterval_end = newInterval[1];
-    int sz_intervals = intervals.size();
-    int last = intervals[sz_intervals - 1][1];
-    vector<int> line(last + 1, -2);
-    unordered_map<string, int> tmp;
-    vector<vector<int>> res;
-
-    for (int i = 0; i < intervals.size(); ++i) {
-      int start = intervals[i][0];
-      int end = intervals[i][1];
-
-      for (int j = start; j <= end; ++j) {
-        line[j] = i;
-      }
-    }
-
-    print_line(line);
-
-    for (int i = newInterval_start; i <= newInterval_end; ++i) {
-      line[i] = -1;
-    }
-
-    print_line(line);
-
-    for (int i = 0; i < last + 1;) {
-      if (line[i] >= 0) {
-        string tmp_ins = to_string(intervals[line[i]][0]) + '-' +
-                         to_string(intervals[line[i]][1]);
-        tmp[tmp_ins] = 1;
-        ++i;
-      } else if (line[i] == -2) {
-        ++i;
-      } else if (line[i] == -1) {
-        int left_end = i;
-        int right_end = i;
-        vector<int> mergedInterval = {-1, -1};
-
-        while (line[left_end] == -1) {
-          left_end--;
+  private:
+  public:
+    vector<vector<int>> insert(vector<vector<int>> &intervals, vector<int> &newInterval) {
+        if (intervals.size() == 0) {
+            return {newInterval};
         }
 
-        while (line[right_end] == -1) {
-          right_end++;
+        int newInterval_start = newInterval[0];
+        int newInterval_end = newInterval[1];
+        vector<vector<int>> res;
+
+        int merge_start = -1, merge_end = -1;
+        for (auto interval : intervals) {
+            int curr_start = interval[0];
+            int curr_end = interval[1];
+
+            if (merge_start != -1 and merge_end != -1) {
+                res.push_back({merge_start, merge_end});
+                merge_start = merge_end = -2;
+            }
+
+            if (newInterval_start > curr_end or newInterval_end < curr_start) {
+
+                if (merge_start != -1 and merge_end == -1) {
+                    merge_end = newInterval_end;
+                    res.push_back({merge_start, merge_end});
+                    merge_start = merge_end = -2;
+                }
+
+                res.push_back(interval);
+                continue;
+            }
+
+            if (newInterval_start >= curr_start and newInterval_end <= curr_end) {
+                res.push_back(interval);
+                merge_start = merge_end = -2;
+                continue;
+            }
+
+            if (newInterval_start >= curr_start) {
+                merge_start = curr_start;
+                continue;
+            }
+
+            if (newInterval_end <= curr_end) {
+                merge_end = curr_end;
+                continue;
+            }
         }
 
-        if (line[left_end] == -2) {
-          mergedInterval[0] = left_end + 1;
-        } else {
-          mergedInterval[0] = intervals[line[left_end]][0];
-          string tmp_ins = to_string(intervals[line[left_end]][0]) + '-' +
-                           to_string(intervals[line[left_end]][1]);
-          cout << "tmp_ins to be deleted: " << tmp_ins << endl;
-          tmp[tmp_ins]--;
+        if (merge_start != -1 and merge_end == -1) {
+            merge_end = newInterval_end;
+            res.push_back({merge_start, merge_end});
         }
 
-        if (line[right_end] == -2) {
-          mergedInterval[1] = right_end - 1;
-          i = right_end;
-        } else {
-          mergedInterval[1] = intervals[line[right_end]][1];
-          string tmp_ins = to_string(intervals[line[right_end]][0]) + '-' +
-                           to_string(intervals[line[right_end]][1]);
-          cout << "tmp_ins to be deleted: " << tmp_ins << endl;
-          tmp[tmp_ins]--;
-          i = right_end;
-          while (line[i] == line[right_end]) {
-            ++i;
-          }
+        if (merge_start == -1 and merge_end == -1) {
+            res.push_back({newInterval_start, newInterval_end});
         }
 
-        string tmp_ins =
-            to_string(mergedInterval[0]) + '-' + to_string(mergedInterval[1]);
-        tmp[tmp_ins]++;
-      }
+        return res;
     }
-
-    print_map(tmp);
-    for (auto iter = tmp.begin(); iter != tmp.end(); iter++) {
-      if (iter->second == 1) {
-        res.push_back(getInterval(iter->first));
-      }
-    }
-    return res;
-  }
 };
 
 int main() {
-  vector<vector<int>> intervals = {{1, 3}, {6, 9}};
-  vector<int> newInterval = {2, 5};
+    // vector<vector<int>> intervals = {{1, 3}, {6, 9}};
+    // vector<int> newInterval = {2, 5};
 
-//   vector<vector<int>> intervals = {{1, 2}, {3, 5}, {6, 7}, {8, 10}, {12, 16}};
-//   vector<int> newInterval = {4, 8};
+    vector<vector<int>> intervals = {{1, 2}, {3, 5}, {6, 7}, {8, 10}, {12, 16}};
+    vector<int> newInterval = {4, 8};
 
-  Solution s;
-  vector<vector<int>> res = s.insert(intervals, newInterval);
-  reverse(res.begin(), res.end());
+    Solution s;
+    vector<vector<int>> res = s.insert(intervals, newInterval);
 
-  cout << "Result: " << endl;
-  for (auto r : res) {
-    for (int ij : r) {
-      cout << ij << " ";
+    cout << "Result: " << endl;
+    for (auto r : res) {
+        for (int ij : r) {
+            cout << ij << " ";
+        }
+        cout << endl;
     }
-    cout << endl;
-  }
 }

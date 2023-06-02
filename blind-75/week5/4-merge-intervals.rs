b4803fc68fbc291e8c00f25b4a0309d4bd19b289
SSJ <jhashubhamshekhar@gmail.com>
2023-06-02 01:18:18 -0500

    week5-4.rs optimized

--- blind-75/week5/4-merge-intervals.rs
+++ blind-75/week5/4-merge-intervals.rs
@@ -28,40 +28,26 @@ Constraints:
 struct Solution();
 impl Solution {
     fn overlap(curr: &Vec<i32>, next: &Vec<i32>) -> bool {
-        let curr_end: i32 = curr[1];
-        let next_start: i32 = next[0];
-        let next_end: i32 = next[1];
-
-        (next_start <= curr_end) || (next_end <= curr_end)
+        (next[0] <= curr[1]) || (next[1] <= curr[1])
     }
 
     pub fn merge(intervals: Vec<Vec<i32>>) -> Vec<Vec<i32>> {
-        let i_len: usize = intervals.len();
         let mut intervals_clone = intervals.clone();
-        let mut intervals_clone2 = intervals.clone();
         let mut merged: Vec<Vec<i32>> = Vec::new();
         intervals_clone.sort_by(|a, b| a[0].cmp(&b[0]));
-        intervals_clone2.sort_by(|a, b| a[0].cmp(&b[0]));
 
-        let mut i: usize = 0;
-        let mut curr_interval: &mut Vec<i32> = &mut intervals_clone[i];
-        while i < i_len {
-            if i < i_len - 1 {
-                let next_interval: &Vec<i32> = &intervals_clone2[i + 1];
-                if Self::overlap(curr_interval, next_interval) {
-                    curr_interval[0] = std::cmp::min(curr_interval[0], next_interval[0]);
-                    curr_interval[1] = std::cmp::max(curr_interval[1], next_interval[1]);
-                    i += 1;
-                } else {
-                    merged.push(curr_interval.to_vec());
-                    i += 1;
-                    curr_interval = &mut intervals_clone[i];
-                }
+        let mut intervals_iter = intervals_clone.iter_mut();
+        let mut curr_interval = intervals_iter.next().unwrap();
+        for interval in intervals_iter {
+            if Self::overlap(&curr_interval, interval) {
+                curr_interval[0] = std::cmp::min(curr_interval[0], interval[0]);
+                curr_interval[1] = std::cmp::max(curr_interval[1], interval[1]);
             } else {
                 merged.push(curr_interval.to_vec());
-                i += 1;
+                curr_interval = interval;
             }
         }
+        merged.push(curr_interval.to_vec());
 
         merged
     }

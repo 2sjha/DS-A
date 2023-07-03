e36fbff27e1f97cfcb07b4b18a74a11d9538035e
SSJ <jhashubhamshekhar@gmail.com>
2023-07-03 17:02:23 -0500

    test commit

--- blind-75/week7/3-word-search.cpp
+++ blind-75/week7/3-word-search.cpp
@@ -38,82 +38,9 @@ Constraints:
 using namespace std;
 
 class Solution {
-  private:
-    bool begin_search(vector<vector<char>> &board, string word, int i, int j) {
-        int curr_i, curr_j, curr_idx;
-        pair<pair<int, int>, int> curr;
-        stack<pair<pair<int, int>, int>> dfs_st;
-        vector<pair<int, int>> visited;
-        dfs_st.push({{i, j}, 0});
-        visited.push_back({i, j});
-
-        while (!dfs_st.empty()) {
-            curr = dfs_st.top();
-            dfs_st.pop();
-
-            curr_i = curr.first.first;
-            curr_j = curr.first.second;
-            curr_idx = curr.second;
-
-            if (curr_idx == word.length() - 1) {
-                return true;
-            }
-
-            bool next_found = false;
-
-            if (curr_i > 0 && !check_visited(visited, curr_i - 1, curr_j) &&
-                board[curr_i - 1][curr_j] == word[curr_idx + 1]) {
-                dfs_st.push({{curr_i - 1, curr_j}, curr_idx + 1});
-                visited.push_back({curr_i - 1, curr_j});
-                next_found = true;
-            }
-            if (curr_j > 0 && !check_visited(visited, curr_i, curr_j - 1) &&
-                board[curr_i][curr_j - 1] == word[curr_idx + 1]) {
-                dfs_st.push({{curr_i, curr_j - 1}, curr_idx + 1});
-                visited.push_back({curr_i, curr_j - 1});
-                next_found = true;
-            }
-            if (curr_i < board.size() - 1 && !check_visited(visited, curr_i + 1, curr_j) &&
-                board[curr_i + 1][curr_j] == word[curr_idx + 1]) {
-                dfs_st.push({{curr_i + 1, curr_j}, curr_idx + 1});
-                visited.push_back({curr_i + 1, curr_j});
-                next_found = true;
-            }
-            if (curr_j < board[0].size() - 1 && !check_visited(visited, curr_i, curr_j + 1) &&
-                board[curr_i][curr_j + 1] == word[curr_idx + 1]) {
-                dfs_st.push({{curr_i, curr_j + 1}, curr_idx + 1});
-                visited.push_back({curr_i, curr_j + 1});
-                next_found = true;
-            }
-
-            if (!next_found) {
-                visited.pop_back();
-            }
-        }
-
-        return false;
-    }
-
-    bool check_visited(vector<pair<int, int>> &visited, int curr_i, int curr_j) {
-        for (pair<int, int> cell : visited) {
-            if (cell.first == curr_i && cell.second == curr_j) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-  public:
-    bool exist(vector<vector<char>> &board, string word) {
-        for (int i = 0; i < board.size(); ++i) {
-            for (int j = 0; j < board[0].size(); ++j) {
-                if (board[i][j] == word[0] && begin_search(board, word, i, j)) {
-                    return true;
-                }
-            }
-        }
-
-        return false;
+public:
+    bool exist(vector<vector<char>>& board, string word) {
+        
     }
 };
 
@@ -153,5 +80,5 @@ int main() {
     cout << res << endl;
     assert(res == true);
 
-    return 0;
+    return 00;
 }

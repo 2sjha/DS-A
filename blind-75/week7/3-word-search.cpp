a1d9648511c00dd195d06bef91b7c1eea4272500
SSJ <jhashubhamshekhar@gmail.com>
2023-07-08 15:57:48 -0500

    finally solved. almost had it, but was stuck for sooo long.

--- blind-75/week7/3-word-search.cpp
+++ blind-75/week7/3-word-search.cpp
@@ -37,10 +37,57 @@ Constraints:
 #include <iostream>
 using namespace std;
 
+// DFS is obvious but
+// I was stuck at detecting chars already traversed/ in dfs stack
+// Solution is to change board before stack call and revert it later
 class Solution {
-public:
-    bool exist(vector<vector<char>>& board, string word) {
-        
+  private:
+    bool find_word_dfs(vector<vector<char>> &board, string word, int row, int col, int idx) {
+        int m = board.size();
+        int n = board[0].size();
+        int len = word.length();
+
+        if (idx == len - 1) {
+            return true;
+        }
+
+        board[row][col] = '#';
+
+        bool l = false, r = false, u = false, d = false;
+
+        if (col < n - 1 && board[row][col + 1] == word[idx + 1]) {
+            r = find_word_dfs(board, word, row, col + 1, idx + 1);
+        }
+        if (row < m - 1 && board[row + 1][col] == word[idx + 1]) {
+            d = find_word_dfs(board, word, row + 1, col, idx + 1);
+        }
+        if (col > 0 && board[row][col - 1] == word[idx + 1]) {
+            l = find_word_dfs(board, word, row, col - 1, idx + 1);
+        }
+        if (row > 0 && board[row - 1][col] == word[idx + 1]) {
+            u = find_word_dfs(board, word, row - 1, col, idx + 1);
+        }
+        board[row][col] = word[idx];
+
+        return l || r || u || d;
+    }
+
+  public:
+    bool exist(vector<vector<char>> &board, string word) {
+        int m = board.size();
+        int n = board[0].size();
+
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                if (board[i][j] == word[0]) {
+                    if (find_word_dfs(board, word, i, j, 0)) {
+                        return true;
+                    }
+                }
+            }
+        }
+
+        return false;
     }
 };
 
@@ -71,14 +118,20 @@ int main() {
     cout << res << endl;
     assert(res == true);
 
-    vector<vector<char>> board3 = {
-        {'A', 'B', 'C', 'E'},
-        {'S', 'F', 'E', 'S'},
-        {'A', 'D', 'E', 'E'}};
-    string word5 = "ABCESEEEFS";
-    res = sol.exist(board3, word5);
+    vector<vector<char>> board4 = {
+        {'A', 'B', 'C', 'E'}, {'S', 'F', 'E', 'S'}, {'A', 'D', 'E', 'E'}};
+    string word6 = "ABCESEEEFS";
+    res = sol.exist(board4, word6);
     cout << res << endl;
     assert(res == true);
 
-    return 000000;
+    vector<vector<char>> board3 = {{'A', 'A', 'A', 'A', 'A', 'A'}, {'A', 'A', 'A', 'A', 'A', 'A'},
+                                   {'A', 'A', 'A', 'A', 'A', 'A'}, {'A', 'A', 'A', 'A', 'A', 'A'},
+                                   {'A', 'A', 'A', 'A', 'A', 'A'}, {'A', 'A', 'A', 'A', 'A', 'A'}};
+    string word5 = "AAAAAAAAAAAAAAa";
+    res = sol.exist(board3, word5);
+    cout << res << endl;
+    assert(res == false);
+
+    return 0;
 }

9a7952c13052ab344e7f3535d9854378057a4f64
Shubham Shekhar Jha <jhashubhamshekhar@gmail.com>
2023-08-22 18:34:06 -0500

    easy questions

---
+++ leetcode-extras/easy/189-rotate-array.cpp
@@ -0,0 +1,92 @@
+/*
+https://leetcode.com/problems/rotate-array/
+
+Given an integer array nums, rotate the array to the right by k steps, where k is non-negative.
+
+
+Example 1:
+
+Input: nums = [1,2,3,4,5,6,7], k = 3
+Output: [5,6,7,1,2,3,4]
+Explanation:
+rotate 1 steps to the right: [7,1,2,3,4,5,6]
+rotate 2 steps to the right: [6,7,1,2,3,4,5]
+rotate 3 steps to the right: [5,6,7,1,2,3,4]
+
+Example 2:
+
+Input: nums = [-1,-100,3,99], k = 2
+Output: [3,99,-1,-100]
+Explanation:
+rotate 1 steps to the right: [99,-1,-100,3]
+rotate 2 steps to the right: [3,99,-1,-100]
+
+
+Constraints:
+
+    1 <= nums.length <= 10^5
+    -231 <= nums[i] <= 231 - 1
+    0 <= k <= 10^5
+
+
+Follow up:
+
+    Try to come up with as many solutions as you can. There are at least three different ways to
+solve this problem. Could you do it in-place with O(1) extra space?
+*/
+
+#include "./../../cpp-utils/printutils.h"
+#include "bits/stdc++.h"
+#include <iostream>
+using namespace std;
+
+class Solution {
+  public:
+    void rotate(vector<int> &nums, int k) {
+        int n = nums.size();
+
+        if (k == 0) {
+            return;
+        }
+
+        int gcd = __gcd(n, k);
+        if (gcd != 1) {
+            int flips = n / gcd;
+            int j = 0;
+
+            for (int h = 0; h < gcd; ++h) {
+                int flipCount = 0;
+                int tmp = nums[h];
+                int i = h;
+                while (flipCount <= flips) {
+                    j = (i + k) % n;
+                    swap(tmp, nums[j]);
+                    i = j;
+                    flipCount++;
+                }
+            }
+        } else {
+            int shifts = 0;
+            int i = 0, j = 0;
+            int tmp = nums[i];
+
+            while (shifts <= n) {
+                j = (i + k) % n;
+                swap(tmp, nums[j]);
+                i = j;
+                shifts++;
+            }
+        }
+    }
+};
+
+int main() {
+    Solution sol;
+
+    vector<int> nums1 = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12};
+    int k1 = 9;
+    sol.rotate(nums1, k1);
+    printArray(nums1);
+
+    return 0;
+}
---
+++ leetcode-extras/easy/205-isomorphic-strings.cpp
@@ -0,0 +1,96 @@
+/*
+Given two strings s and t, determine if they are isomorphic.
+
+Two strings s and t are isomorphic if the characters in s can be replaced to get t.
+
+All occurrences of a character must be replaced with another character while preserving the order of
+characters. No two characters may map to the same character, but a character may map to itself.
+
+
+Example 1:
+
+Input: s = "egg", t = "add"
+Output: true
+
+Example 2:
+
+Input: s = "foo", t = "bar"
+Output: false
+
+Example 3:
+
+Input: s = "paper", t = "title"
+Output: true
+
+
+Constraints:
+
+    1 <= s.length <= 5 * 104
+    t.length == s.length
+    s and t consist of any valid ascii character.
+*/
+
+#include "./../../cpp-utils/printutils.h"
+#include "bits/stdc++.h"
+#include <iostream>
+using namespace std;
+
+class Solution {
+  public:
+    bool isIsomorphic(string s, string t) {
+        int n = s.length();
+        int flips = 0;
+        map<char, char> st_mp;
+
+        for (int i = 0; i < n; ++i) {
+            if (!st_mp.count(s[i])) {
+                st_mp[s[i]] = t[i];
+                flips++;
+            }
+        }
+
+        for (auto iter1 = st_mp.begin(); iter1 != st_mp.end(); iter1++) {
+            auto iter2 = iter1;
+            iter2++;
+            for (; iter2 != st_mp.end(); iter2++) {
+                if (iter1->second == iter2->second) {
+                    return false;
+                }
+            }
+        }
+
+        string s_new = s;
+        for (int i = 0; i < n; ++i) {
+            if (st_mp.count(s_new[i])) {
+                s_new[i] = st_mp[s_new[i]];
+            }
+        }
+
+        return s_new == t;
+    }
+};
+
+int main() {
+    Solution sol;
+    bool res;
+
+    string s1 = "egg";
+    string t1 = "add";
+    res = sol.isIsomorphic(s1, t1);
+    cout << res << endl;
+    assert(res == 1);
+
+    string s2 = "foo";
+    string t2 = "bar";
+    res = sol.isIsomorphic(s2, t2);
+    cout << res << endl;
+    assert(res == 1);
+
+    string s3 = "paper";
+    string t3 = "title";
+    res = sol.isIsomorphic(s3, t3);
+    cout << res << endl;
+    assert(res == 1);
+
+    return 0;
+}
